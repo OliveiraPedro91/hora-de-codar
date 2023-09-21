9db9ff798cc5c071a91a8a62b8e4c5e6d54be403
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-09-21 15:02:16 -0300

    script atualizado

---
+++ curso/JavaScript/2-operadores/script.js
@@ -0,0 +1,23 @@
+// 1- Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -10);
+
+//2 - Operações aritiméticas
+
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(4 / 2);
+console.log(4 * 2);
+console.log(5 + 4 * 2);
+
+// 3 - Special numbers
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "asd");
+
+console.log(typeof NaN);
+
+// 4 - string
