e1d4ddc55b28891f00dfa378991991c04ad8f5fb
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-09-23 14:08:50 -0300

    Update script.js

--- curso/JavaScript/3-estruturas/script/script.js
+++ curso/JavaScript/3-estruturas/script/script.js
@@ -3,3 +3,31 @@ let nome = "Pedro";
 
 console.log (nome)
 
+nome = "Pedro Oliveira da Silva"
+
+console.log(nome)
+
+const idade = 31
+
+console.log(idade) //Constante nao tem como trocar o valor
+
+console.log (typeof nome)
+console.log (typeof idade)
+
+// 2 - Mais sobre variáveis
+
+let a = 10, 
+b = 20, 
+c = 30;
+
+console.log(a, b, c);
+
+const nomeCompleto = "Pedro Oliveira da Silva"
+
+console.log(nomeCompleto);
+console.log(nomeCompleto);
+
+let _teste = "ok";
+let $teste = "ok";
+
+console.log(_teste, $teste);
