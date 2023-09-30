ddc5ec2c8280db54cb9a9e4192c79d029b497b51
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-09-30 13:25:44 -0300

    Update scripts.js

--- curso/JavaScript/4-funções/js/scripts.js
+++ curso/JavaScript/4-funções/js/scripts.js
@@ -18,3 +18,35 @@ function funcaoComParamentro(txt){
 
 funcaoComParamentro("Imprimindo alguma coisa");
 funcaoComParamentro("Outra função");
+
+// 2 - return
+const a = 10;
+const b = 20;
+const c = 30;
+const d = 40;
+function soma(n1, n2){
+    return n1 + n2;
+}
+const resultado = soma(a , b);
+
+console.log(resultado)
+console.log(soma(c, d)); 
+
+//
+// const x = 5;
+// const z = 10;
+
+// function multiplica(x, y){
+//     return x * y;
+// }
+// const total = multiplica(x , y);
+// console.log(total)
+
+// 3 - escopo da função
+let y = 10
+function testandoEscopo(){
+    let y = 20;
+    console.log(`Y dentro da função é: ${y}`);
+}
+testandoEscopo();
+console.log(`Y fora da função é: ${y}`);
