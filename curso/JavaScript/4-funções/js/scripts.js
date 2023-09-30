cb1cf14e551c20e6d1b1494498cbb65abe58be78
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-09-30 13:35:13 -0300

    Update scripts.js

--- curso/JavaScript/4-funções/js/scripts.js
+++ curso/JavaScript/4-funções/js/scripts.js
@@ -50,3 +50,24 @@ function testandoEscopo(){
 }
 testandoEscopo();
 console.log(`Y fora da função é: ${y}`);
+
+// 4 - Escopo aninhado
+let m = 10
+function escopoAninhado(){
+    let m = 20;
+
+    if(true){
+        let m = 30;
+
+            if(true){
+                let m = 40;
+
+            console.log(m);
+            }
+
+    console.log(m);
+    }
+console.log(m);
+}
+escopoAninhado();
+console.log(m);
