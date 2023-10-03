259f82b1a682a831518972e064726a6d85f0823e
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-03 11:07:31 -0300

    Update scripts.js

--- curso/JavaScript/5-arrays e objetos/js/scripts.js
+++ curso/JavaScript/5-arrays e objetos/js/scripts.js
@@ -7,9 +7,14 @@ const itens = ["Pedro", true, 2, 4.12,[]];
 console.log(itens);
 console.log(typeof itens);
 
-// 2 - propriedades
+// 2 - Mais sobre arrays
+const arr = ["primeiro item", "segundo item","terceiro item","quarto item"]
+console.log(arr[2]);
+
+// 3 - propriedades
 const number = [5, 3, 7]
 console.log(number.length);
 console.log(number["length"]);
 
-//  
+const myName = "Pedro";
+console.log(myName.length);
