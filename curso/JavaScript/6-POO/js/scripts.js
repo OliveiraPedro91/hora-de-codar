52e6467d9c378acdb3af7d8216b474dc3a9f2ac8
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-19 15:49:01 -0300

    Update scripts.js

--- curso/JavaScript/6-POO/js/scripts.js
+++ curso/JavaScript/6-POO/js/scripts.js
@@ -1,11 +1,30 @@
 // 1 - Métodos
-const animal = {
-    nome: "bob"
-    latir: function(){
-        console.log("au au")
-    }
-};
+//const animal = {
+ //   nome: "bob"
+ //   latir: function(){
+ //       console.log("au au")
+ //   }
+//};
 
-console.log(animal.nome);
-animal.latir();
-//
+//console.log(animal.nome);
+//animal.latir();
+
+// 2 - Aprofundando em métodos
+const pessoa = {
+    nome: "Pedro",
+    getNome: function(){
+        return this.nome;
+    },
+    setNome: function(novoNome){
+        this.nome = novoNome;
+    },
+};
+console.log(pessoa.nome);
+console.log(pessoa.getNome());
+pessoa.setNome("Joaquin");
+console.log(pessoa.getNome());
+
+// 3 - prototype
+const text = "asd"
+
+console.log(Object.getPrototypeOf(text));
