5254ab9a895f99651bae12109a6fc2b66315fa2d
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-19 17:46:17 -0300

    Update scripts.js

--- curso/JavaScript/6-POO/js/scripts.js
+++ curso/JavaScript/6-POO/js/scripts.js
@@ -98,3 +98,18 @@ class CachorroClasse{
 }
 const jeff = new CachorroClasse("Jeff", "Labrador")
 console.log(jeff);
+
+// 10 - mais sobre classes
+class Caminhao{
+    constructor(eixos, cor){
+        this.eixos = eixos;
+        this.cor = cor;
+    }
+    descreverCaminhao(){
+        console.log(`Este caminhão tem ${this.eixos} eixos e é da cor ${this.cor}`);
+    }
+}
+
+const scania = new Caminhao(6, "Vermelha");
+console.log(scania);
+scania.descreverCaminhao();
