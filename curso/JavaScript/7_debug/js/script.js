06a99bf4af8c2425f55bbf34ee1875d6aac3bd11
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-21 09:20:32 -0300

    Update script.js

--- curso/JavaScript/7_debug/js/script.js
+++ curso/JavaScript/7_debug/js/script.js
@@ -3,3 +3,18 @@
 
 //opa = "teste";
 const opa = "teste";
+
+// 2 - consoloe.log
+let a = 1
+let b = 2
+    if(a==1){
+        a=b+2;
+    }
+console.log(a);
+
+for(let i = 0; i < b; i++){
+    a = a + 2;
+    console.log(a);
+}
+
+// 3 - debugger
