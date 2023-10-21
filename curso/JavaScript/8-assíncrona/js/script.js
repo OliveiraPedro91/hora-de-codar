91e22730f2bee3e308f906ef5bf6a70bf1bfaafa
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-21 13:54:03 -0300

    Update script.js

--- curso/JavaScript/8-assíncrona/js/script.js
+++ curso/JavaScript/8-assíncrona/js/script.js
@@ -13,3 +13,17 @@ console.log("Ainda não começou")
 setInterval(function(){
     console.log("Intervalo assíncrono");
 }, 3000);
+
+// 3 - promise
+const promessa = Promise.resolve(5+5);
+
+console.log("Algum código");
+
+promessa.then((value)=> {
+    console.log(`A soma é ${value}`);
+    return value;
+})
+.then((value)=>value -1)
+.then((value) => console.log(`Agora é ${value}`));
+
+console.log("Outro código");
