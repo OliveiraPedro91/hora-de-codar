1816f3535ce7f1af1aa13e9cd5b6557538334c93
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-22 14:47:03 -0300

    Update script.js

--- curso/JavaScript/8-assíncrona/js/script.js
+++ curso/JavaScript/8-assíncrona/js/script.js
@@ -81,3 +81,18 @@ somarComDelay(2,4).then((value) =>{
     console.log(`O valor da soma é: ${value}`);
 });
 console.log("teste async");
+
+// 8 - async await
+function resolveComDelay(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Resolveu a Promise");
+        }, 2000);
+    });
+}
+async function chamadaAsync(){
+    console.log("Chamado a promise, e esperando o resultado");
+    const result = await resolveComDelay();
+    console.log(`O resultado chegou: ${result}`);
+}
+chamadaAsync();
