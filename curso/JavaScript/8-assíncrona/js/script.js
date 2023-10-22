9602c6c04802ecde3b01478436b547f02f4952cc
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-22 14:33:42 -0300

    Update script.js

--- curso/JavaScript/8-assíncrona/js/script.js
+++ curso/JavaScript/8-assíncrona/js/script.js
@@ -55,3 +55,22 @@ a.then((v) => console.log(`O resultado é ${v}`)).catch((err) => console.log*(`U
 );
 b.then((v) => console.log(`O resultado é ${v}`)).catch((err) => console.log*(`Um erro ocorreu: ${err}`)
 );
+
+// 6 - resolvendo várias promises
+const p1 = new Promise((resolve, reject) =>{
+ setTimeout(function(){
+    resolve(10)
+},1000)
+})
+const p2 = Promise.resolve(10+10);
+const p3 = new Promise ((resolve, reject) => {
+    if (30 > 10){
+        resolve(30)
+    }else{
+        reject("Erro!")
+    }
+})
+
+Promise.all([p1, p2, p3]).then((values) => console.log(values));
+
+//
