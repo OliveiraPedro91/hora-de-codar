c2057dcd38d45191204b49ea19a7a672fbd16e83
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-22 15:52:59 -0300

    links atualizados

--- curso/JavaScript/9-dom/js/script.js
+++ curso/JavaScript/9-dom/js/script.js
@@ -1,3 +1,4 @@
 // 1 - Movendo-se pelo DOM
 console.log(document.body);
-console.log(document.body.childNodes)
+console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[1].childNodes);
