2d30671a33ad5174b5ac1c5e0800378b9dfa4fd4
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-10-23 16:39:37 -0300

    html, scripts e css atualizados

--- curso/JavaScript/10-eventos/script/script.js
+++ curso/JavaScript/10-eventos/script/script.js
@@ -19,10 +19,38 @@ thirdBtn.addEventListener("click", () => {
 })
 
 // 3 - Argumento do evento
-const myTitle = document.querySelector("#my-title")
-myTitle.addEventListener("click", (event) => {
-    console.log(event)
+// const myTitle = document.querySelector("#my-title")
+// myTitle.addEventListener("click", (event) => {
+//     console.log(event)
+// })
+
+//4 - propagação
+const containerBtn = document.querySelector("#btn-container");
+const btnInsiderContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1")
 })
 
-//4 - 
+btnInsiderContainer.addEventListener("click", (e) => {
+    e.stopPropagation()
+    console.log("Evento 2")
+})
+
+// 5 - removendo evento padrão
+const a = document.querySelector("a")
+a.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log("Não alterou a página")
+})
+
+// 6 - Eventos de tecla
+document.addEventListener("keyup", (e) =>{
+    console.log(`Soltou a tecla ${e.key}`);
+});
+
+document.addEventListener("keyup", (e) =>{
+    console.log(`Soltou a tecla ${e.key}`);
+});
 
