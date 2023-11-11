3574af86fa4703fa0c456152b15427bd8fea2e67
Pedro Oliveira da Silva <pedrooliveira131@gmail.com>
2023-11-11 06:35:22 -0300

    js atualizado

---
+++ curso/03-Projetos/02-GeradorQrCode/script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+const qrCodeInput = document.querySelector("#qr-form input")
+const qrCodeImg = document.querySelector("#qr-code img")
+
+// Eventos
+function generateQrCode(){
+    const qrCodeInputValue = qrCodeInput.value; 
+    console.log(qrCodeInputValue)
+    if (!qrCodeInputValue) return;
+    qrCodeBtn.innerText = "Gerando código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
+
+    container.classList.add("active");
+
+}
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode();
+});
